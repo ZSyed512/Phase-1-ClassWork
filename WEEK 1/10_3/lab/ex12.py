2aa81c063f7492e36328559b4ad1ee0502394ef5
ZSyed512 <zaighamasyed@gmail.com>
2023-12-12 09:16:21 -0500

    INITIAL PUSH

---
+++ WEEK 1/10_3/lab/ex12.py
@@ -0,0 +1,17 @@
+# Exercise 1.2. Write Python code to evaluate these expressions.
+# The answers can be stored in a name, or evaluated in a print statement
+
+# 1. How many seconds are there in 42 minutes 42 seconds?
+
+SEC = (42*60+42)
+
+# 2. How many miles are there in 10 kilometers? Hint: there are 1.61
+# kilometers in a mile.
+
+MILES = (10/1.61)
+
+# 3. If you run a 10 kilometer race in 42 minutes 42 seconds, what is your
+# average pace (time per mile in minutes and seconds)? What is your average
+# speed in miles per hour?
+
+AVG = (MILES/SEC)
